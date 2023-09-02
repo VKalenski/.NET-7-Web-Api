eae15edf1e99b3cad1fd68e4b678672c6baed9e1
Vilislav Kalenski <71873406+VKalenski@users.noreply.github.com>
2023-09-02 12:28:23 +0300

    Add AutomapperProfile.cs and modified appsettings.json and dotnet-rpg.csproj

---
+++ dotnet-rpg/AutoMapperProfile.cs
@@ -0,0 +1,18 @@
+using dotnet_rpg.Dtos.Fight;
+using dotnet_rpg.Dtos.Skill;
+using dotnet_rpg.Dtos.Weapon;
+
+namespace dotnet_rpg
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<Character,GetCharacterDto>();
+            CreateMap<AddCharacterDto,Character>();
+            CreateMap<Weapon,GetWeaponDto>();
+            CreateMap<Skill, GetSkillDto>();
+            CreateMap<Character, HighscoreDto>();
+        }
+    }
+}
